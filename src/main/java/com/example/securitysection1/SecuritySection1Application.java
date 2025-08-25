237c841251089270ed3d60a2791d7ea74a3c8f3d
Pavel Bukharov <pbuxaroff@gmail.com>
2025-08-25 20:22:21 +0300

    first commit

---
+++ src/main/java/com/example/securitysection1/SecuritySection1Application.java
@@ -0,0 +1,13 @@
+package com.example.securitysection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySection1Application.class, args);
+	}
+
+}
