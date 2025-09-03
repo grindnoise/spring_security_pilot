3ab36b8b7e62ecd49f36890ec32a8e789d47bac9
p.bukharov <p.bukharov@rendez-vous.ru>
2025-09-03 18:50:11 +0300

    sec2

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -2,18 +2,23 @@ package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
         http.authorizeHttpRequests(requests ->
                 requests.requestMatchers(
                                 "/myBalance",
@@ -29,4 +34,22 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+//        return new InMemoryUserDetailsManager(
+//                User.withUsername("user").password("{noop}EazyBytes@12345").roles("read").build(),
+//                User.withUsername("admin").password("{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m").roles("admin").build());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // Api checking
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
 }
---
+++ src/main/resources/sql/scripts.sql
@@ -0,0 +1,9 @@
+create table `eazybank`.users(username varchar(50) not null primary key,password varchar(500) not null,enabled boolean not null);
+create table `eazybank`.authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
+create unique index ix_auth_username on `eazybank`.authorities (username,authority);
+
+INSERT IGNORE INTO `eazybank`.`users` VALUES ('user', '{noop}EazyBytes@12345', '1');
+INSERT IGNORE INTO `eazybank`.`authorities` VALUES ('user', 'read');
+
+INSERT IGNORE INTO `eazybank`.`users` VALUES ('admin', '{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m', '1');
+INSERT IGNORE INTO `eazybank`.`authorities` VALUES ('admin', 'admin');
